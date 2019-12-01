02e8273a9b165217d23f590f76ee30c44f58f2ce
יונתן גואל <yonatangoel@ywntns-MacBook-Pro.local>
2019-12-01 22:56:59 +0200

    Updates

---
+++ precache-manifest.3837f5d29ae0459f82cb9178487af2d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7579b4b51d37f153933ba0f6feee14",
+    "url": "/new-app/index.html"
+  },
+  {
+    "revision": "e788fffc1c5ce2308a36",
+    "url": "/new-app/static/css/main.8fd75e0c.chunk.css"
+  },
+  {
+    "revision": "439ccc6c6bd9a6715c23",
+    "url": "/new-app/static/js/2.491da432.chunk.js"
+  },
+  {
+    "revision": "e788fffc1c5ce2308a36",
+    "url": "/new-app/static/js/main.326a80bc.chunk.js"
+  },
+  {
+    "revision": "bb10f749488b8d7e272d",
+    "url": "/new-app/static/js/runtime-main.cba064e1.js"
+  }
+]);
